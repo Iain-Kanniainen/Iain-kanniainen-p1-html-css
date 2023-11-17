0b79c4d8947ac3c74163496b4c02ca3ffe74c4d5
Iain-Kanniainen <9022209@student.zadkine.nl>
2023-11-17 14:12:59 +0100

    7.1

---
+++ JS-7.1-main/app.js
@@ -0,0 +1,71 @@
+function calculate(){
+    let a = parseFloat(prompt("vul uw budget in"));
+    let b = parseFloat(prompt("vul de kosten in"));
+    if (a < b){
+        alert ("Helaas, te weinig geldt (<- misspelling on the site)")
+    } else {
+        alert("U heeft genoeg geld!")
+    };
+}
+
+//this is just nicer than making a lot of files
+
+let clickercount = 0;
+function clicker(){
+    clickercount ++;
+    let thingamajig = document.getElementById("clicker");
+    thingamajig.textContent = "clicker count: " + clickercount;
+    if (clickercount > 10){
+        clickercount = 0;
+    }
+}
+
+let answer;
+function AddNumber(){
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
+    answer = document.getElementById('answerCalc');
+    if (a > 0 && b > 0){
+    answer.textContent = a + b;
+    } else {
+        answer.textContent = "both need to be greater than 0";
+    }
+    return false;
+    
+}
+
+function SubNumber(){
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
+    answer = document.getElementById('answerCalc');
+    if (a > 0 && b > 0){
+    answer.textContent = a - b;
+    } else {
+        answer.textContent = "both need to be greater than 0";
+    }
+    return false;
+}
+
+function MultNumber(){
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
+    answer = document.getElementById('answerCalc');
+    if (a > 0 && b > 0){
+    answer.textContent = a * b;
+    } else {
+        answer.textContent = "both need to be greater than 0";
+    }
+    return false;
+}
+
+function DivNumber(){
+    let a = parseFloat(document.getElementById("number1").value);
+    let b = parseFloat(document.getElementById("number2").value);
+    answer = document.getElementById('answerCalc');
+    if (a > 0 && b > 0){
+    answer.textContent = a / b;
+    } else {
+        answer.textContent = "both need to be greater than 0";
+    }
+    return false;
+}
